40c46bc5bdbbafd906055d7993536dc0f7cac9be
Jon21paulos <83462298+Jon21paulos@users.noreply.github.com>
2021-07-28 22:06:49 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -46,7 +46,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href="https://mail.google.com" className="text-indigo-400 leading-relaxed">
                 yohannespaulos1616@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
